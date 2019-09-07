5967cdaa3f6fd7786212507a495a726e2a1f2941
Christian Schmitz <ChristianSchmitz@uol.com.br>
2019-09-07 15:22:41 -0300

    Added input file reading capabilities.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,17 @@
+#define ERROR_CODE 3
+
 int main(int argc, char** argv){
+    if (argc < 2){
+        printf("Please call: ./etapa2 input.txt\n");
+        exit(ERROR_CODE);
+    }
+
+    yyin = fopen(argv[1], "r");
+    if (yyin == NULL) {
+        printf("Could not open file %s.\n", argv[1]);
+        exit(ERROR_CODE);
+  }
+
     hashInit();
     yyparse();
     hashPrint();
